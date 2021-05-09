b162f91234acb3df1c9b8a825ae7d2ec5ff7f1db
Mathijs <mathijsmarks@msn.com>
2021-05-09 14:34:58 +0200

    Initial commit.

---
+++ Assets/Editor/SomeGreenTest.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+public class SomeGreenTest : MonoBehaviour
+{
+    [Test]
+    public void SomeGreenTestPassing()
+    {
+        Assert.Pass();
+    }
+}
